7400efd1cb162856f3cc5c462ec4f97364795511
Jakub Kropáček <kropikuba@gmail.com>
2021-04-13 09:14:51 +0200

    Added mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const timeElement = document.getElementById("time");
+const nav = document.getElementsByTagName("nav")[0];
+const childNav = nav.getElementsByTagName("ul")[1].getElementsByTagName("li");
+const showNav = document.getElementById("showNav");
 
+const hideNav = () => {
+	if (document.body.clientWidth < 750) {
+		nav.style.display = "";
+	}
+};
 
 var intervalID = setInterval(() => {
 	let date = new Date();
@@ -24,3 +32,14 @@ if (timeElement == null) {
 	clearInterval(intervalID);
 }
 
+showNav.addEventListener("click", () => {
+	if (nav.style.display === "table") {
+		nav.style.display = "";
+	} else {
+		nav.style.display = "table";
+	}
+});
+
+for (var i = 0; i < childNav.length; i++) {
+	childNav[i].addEventListener("click", hideNav);
+}
