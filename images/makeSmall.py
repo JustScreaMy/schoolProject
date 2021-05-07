1bc2a286a50487e7b81d956db452102361228ef5
Jakub Kropáček <kropikuba@gmail.com>
2021-05-07 12:51:36 +0200

    Doplnena galerie, pridan recept

--- images/makeSmall.py
+++ images/makeSmall.py
@@ -4,7 +4,8 @@ from PIL import Image
 for filename in os.listdir('.'):
     if filename.endswith((".jpg", ".jpeg")):
         with Image.open(filename) as img:
-            img_ratio = img.width / img.height
+            if(img.width < img.height):
+                img = img.transpose(Image.ROTATE_90)
             resized_img = img.resize((320, 180))
             resized_img.save("thumb_"+filename)
         print("Compressed", filename)
